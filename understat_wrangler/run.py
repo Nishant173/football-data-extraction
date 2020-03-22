cc6379ce68ea3e059d7ef4155c7dd903038be63d
Nishant173 <nishant.rao173@gmail.com>
2020-03-23 02:27:52 +0530

    Added try/except block

--- understat_wrangler/run.py
+++ understat_wrangler/run.py
@@ -3,6 +3,9 @@ from pipeline import execute_pipeline
 
 
 if __name__ == "__main__":
-    time_taken = utils.run_and_timeit(func=execute_pipeline)
-    print("Time taken: {} minutes".format(time_taken))
-    print("Data has been extracted and wrangled!")
+    try:
+        time_taken = utils.run_and_timeit(func=execute_pipeline)
+        print("Time taken: {} minutes".format(time_taken))
+        print("Data has been extracted and wrangled!")
+    except Exception as e:
+        print("Failed! ErrorMsg: {}".format(e))
