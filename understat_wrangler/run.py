574e443167151abcd2c66cc84db22789454a35a8
Nishant173 <nishant.rao173@gmail.com>
2020-07-30 22:59:28 +0530

    Updated run.py

--- understat_wrangler/run.py
+++ understat_wrangler/run.py
@@ -1,11 +1,9 @@
 import utils
 from pipeline import execute_pipeline
 
-
 if __name__ == "__main__":
     try:
-        time_taken = utils.run_and_timeit(func=execute_pipeline)
-        print("Time taken: {} minutes".format(time_taken))
+        utils.run_and_timeit(func=execute_pipeline)
         print("Data has been extracted and wrangled!")
     except Exception as e:
         print("Failed! ErrorMsg: {}".format(e))
